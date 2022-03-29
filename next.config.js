c1607fc2231007895fc9b94287293dd0cd7c82c9
Maxime Kubik <mkubdev@gmail.com>
2022-03-29 16:56:18 +0200

    🔧 update next.js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,51 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const frontmatterPlugin = require('./lib/frontmatter')
 
-module.exports = nextConfig
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    // To use frontmatter, add the plugin to the options
+    // remarkPlugins: [frontmatterPlugin],
+    remarkPlugins: [],
+    rehypePlugins: [],
+
+    // If you use `MDXProvider`, uncomment the following line.
+    providerImportSource: "@mdx-js/react",
+  }
+})
+
+module.exports = withMDX({
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  swcMinify: true,
+  resolve: {
+    fallback: {
+      fs: false,
+    },
+  },
+  images: {
+    domains: [
+      '',
+    ],
+  },async redirects() {
+    return [
+      {
+        source: '/projects',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/mkubdev',
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/mkubdev',
+        permanent: true,
+        basePath: false,
+      },
+    ]
+  },
+  // target: 'serverless'
+})
+
